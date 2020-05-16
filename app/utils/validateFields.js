b81a2eaf04bb01839755b8ea8bb7c31ae5006c5e
ogbk <ogbu.k.olu@gmail.com>
2020-05-16 20:24:16 +0100

    validate email

--- app/utils/validateFields.js
+++ app/utils/validateFields.js
@@ -3,36 +3,56 @@
 const EMPTY_STRING = '';
 
 const validateName = (_value: string): string => {
-  let error = EMPTY_STRING;
   const value = _value.trim();
 
   if (!value || value === EMPTY_STRING) {
-    error = 'Name is required';
+    return ('Name is required');
   }
 
-  return error;
+  return EMPTY_STRING;
 };
 
 const validateEmail = (_value: string): string => {
-  let error = EMPTY_STRING;
-  const value = _value.trim();
+  const valueStr = _value.trim();
 
-  if (!value || value === EMPTY_STRING) {
-    error = 'Email is required';
+  if (!valueStr || valueStr === EMPTY_STRING) {
+    return ('Email is required');
   }
 
-  return error;
+  const valueArr = valueStr.split('');
+  if (valueArr.filter((v) => (v === '@')).length !== 1
+    || valueStr.startsWith('.')
+    || valueStr.endsWith('.')
+  ) {
+    return ('Email is not valid');
+  }
+
+  const fullDomain = valueStr.split('@')[1];
+  const host = fullDomain.slice(0, fullDomain.lastIndexOf('.'));
+  const suffix = fullDomain.slice(fullDomain.lastIndexOf('.') + 1);
+
+  if (!fullDomain
+    || !host
+    || !suffix
+    || !fullDomain.includes('.')
+    || suffix.length < 2
+    || host.length < 2
+    || host.startsWith('.')
+  ) {
+    return ('Email is not valid');
+  }
+
+  return EMPTY_STRING;
 };
 
 const validatePassword = (_value: string): string => {
-  let error = EMPTY_STRING;
   const value = _value.trim();
 
   if (!value || value === EMPTY_STRING) {
-    error = 'Password is required';
+    return ('Password is required');
   }
 
-  return error;
+  return EMPTY_STRING;
 };
 
 export { validateName, validateEmail, validatePassword };
